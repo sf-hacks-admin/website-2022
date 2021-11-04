a8b4f6139d7c3278c6419078bc8e6234af594b82
Anmol Burmy <anmolburmy145@gmail.com>
2021-11-03 20:33:24 -0700

    redoing subheading css

--- app/src/components/placeholder/placeholder.js
+++ app/src/components/placeholder/placeholder.js
@@ -36,12 +36,15 @@ const placeholder = () => {
     return (
         <div className="placeholder-container">
             <div className="heading-container">
+                <div id="main-heading" className="resp-line1">
+                    SF HACKS
+                </div>
+                <div id="main-heading-resp">
+                    <span className="main-heading-resp-line1">SF</span> <span className="main-heading-resp-line2"> HACKS</span>
+                </div>
                 <div id="sub-heading" className="scrolling">
                     SF HACKS CONSTITUTES DIVERGENCE / AN INCLUSIVE PLACE THAT HELPS PEOPLE FIT IN AND FEEL SAFE / WE HELP DISCOVER
                     WHAT INNOVATIVE TECHNOLOGY HAS TO OFFER
-                </div>
-                <div id="main-heading" className="resp-line1 resp-line2">
-                    SF HACKS
                 </div>
             </div>
 
