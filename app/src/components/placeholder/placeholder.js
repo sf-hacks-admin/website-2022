59d2bfdf2c53efa63888d23bb31eb2f4e0c5f790
Anmol Burmy <anmolburmy145@gmail.com>
2021-11-30 19:35:34 -0800

    finally fixed

--- app/src/components/placeholder/placeholder.js
+++ app/src/components/placeholder/placeholder.js
@@ -1,11 +1,14 @@
 import React from "react";
 import "./style.css";
+import "./style-apple.css";
 import facebookIcon from "../../assets/social-links/fb-icon.svg";
 import instagramIcon from "../../assets/social-links/ig-icon.svg";
 import twitterIcon from "../../assets/social-links/ttr-icon.svg";
 import linkedinIcon from "../../assets/social-links/lnk-icon.svg";
 import discordIcon from "../../assets/social-links/dc-icon.svg";
 import mailgo from "mailgo";
+import { isMacOs, isIOS } from "react-device-detect";
+
 const placeholder = () => {
     mailgo();
     const socials = [
@@ -35,6 +38,55 @@ const placeholder = () => {
             link: "https://twitter.com/sf_hacks?lang=en",
         },
     ];
+
+    if (isMacOs || isIOS)
+        return (
+            <div className="placeholder-container-mac">
+                <div className="heading-container-mac">
+                    <div className="main-heading-mac">SF HACKS</div>
+                    <div id="sub-heading-mac">
+                        SF HACKS CONSTITUTES DIVERGENCE / AN INCLUSIVE PLACE THAT HELPS PEOPLE FIT IN AND FEEL SAFE / WE HELP
+                        DISCOVER WHAT INNOVATIVE TECHNOLOGY HAS TO OFFER
+                    </div>
+                </div>
+
+                <div className="heading-container-resp-mac">
+                    <div className="main-heading-resp1-mac">SF</div>
+                    <div className="main-heading-resp2-mac">HACKS</div>
+                    <div className="sub-resp-cont-mac">
+                        <div id="sub-heading-resp-mac" className="scrolling-mac">
+                            SF HACKS CONSTITUTES DIVERGENCE / AN INCLUSIVE PLACE THAT HELPS PEOPLE FIT IN AND FEEL SAFE / WE HELP
+                            DISCOVER WHAT INNOVATIVE TECHNOLOGY HAS TO OFFER
+                        </div>
+                    </div>
+                </div>
+
+                <div className="sub-info-mac">
+                    <div className="info-question-mac">what do we do?</div>
+                    <div className="info-answer-mac">we represent an ambitious inclusive community and empower them.</div>
+                </div>
+
+                <div className="coming-soon-mac">MORE COMING SOON</div>
+                <div className="footer-mac">
+                    <div className="footer-style-mac">
+                        {socials.map((social) => (
+                            <div key={social.name}>
+                                <a href={social.link} target="_blank" rel="noreferrer">
+                                    <img src={social.icon} className="social-icons-mac" alt="" />
+                                </a>
+                            </div>
+                        ))}
+                    </div>
+
+                    <div className="footer-email-mac">
+                        <span className="resp-footer-mac">|</span>
+                        <a class="light-mac" href="mailto:sfhacksteam@gmail.com">
+                            sfhacksteam@gmail.com
+                        </a>
+                    </div>
+                </div>
+            </div>
+        );
     return (
         <div className="placeholder-container">
             <div className="heading-container">
