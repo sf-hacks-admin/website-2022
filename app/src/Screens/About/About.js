949034f28016cccdbb5453383588732d00a66c8b
Burmy <62005549+Burmy@users.noreply.github.com>
2022-01-26 16:12:20 -0800

    working on bugs

--- app/src/Screens/About/About.js
+++ app/src/Screens/About/About.js
@@ -23,10 +23,10 @@ const About = () => {
                     <div className="about-banner-text-center" style={{ transform: `translateY(-${offsetY * 0.1}px)` }}>
                         GOING VIRTUAL
                     </div>
-                    <div className="about-banner-text-bottom" style={{ transform: `translateY(-${offsetY * 0.1}px)` }}>
+                    <div className="about-banner-text-bottom-mac" style={{ transform: `translateY(-${offsetY * 0.1}px)` }}>
                         GOING VIRTUAL
                     </div>
-                    <div className="about-banner-text-mobile" style={{ transform: `translateY(-${offsetY * 0.1}px)` }}>
+                    <div className="about-banner-text-mobile-mac" style={{ transform: `translateY(-${offsetY * 0.1}px)` }}>
                         GOING VIRTUAL
                     </div>
                 </div>
@@ -36,6 +36,33 @@ const About = () => {
 
                 <div className="about-small-cross-cont" style={{ transform: `translateY(-${offsetY * 0.05}px)` }}>
                     <div className="about-small-cross"></div>
+                </div>
+
+                <div className="about-cont">
+                    <div className="about-wrapper">
+                        <div className="about-head-mac">
+                            <div className="about-head-one-mac">ABOUT</div>
+                            <div className="about-head-container">
+                                <div className="about-head-two-mac">SF HACKS</div>
+                                <div className="about-head-two-line-mac">s</div>
+                            </div>
+                        </div>
+
+                        <div className="about-para">
+                            <div className="about-para-cont">
+                                <div className="about-para-one">
+                                    Our organization’s <strong className="orange">purpose</strong> is to plan and host
+                                    California’s most talented collegiate hackers, designers, and developers, to a{" "}
+                                    <strong>3 day hackathon.</strong>
+                                </div>
+                                <div className="about-para-two">
+                                    We work towards giving students an environment for{" "}
+                                    <strong className="orange">maximum innovation, collaboration,</strong> and{" "}
+                                    <strong className="orange">education.</strong>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         );
