843ffea5d7286954445bfb7152529c199a6aa58c
Anmol Burmy <anmolburmy145@gmail.com>
2022-03-03 21:52:34 -0800

    working on calender

--- app/src/Screens/Schedule/Schedule.js
+++ app/src/Screens/Schedule/Schedule.js
@@ -1,12 +1,46 @@
-import React from "react";
+import React, { useState, useEffect, useMemo } from "react";
 import { Tab, Tabs, TabList, TabPanel } from "react-tabs";
 import "react-tabs/style/react-tabs.css";
+import { Calendar, momentLocalizer, Views } from "react-big-calendar";
+import moment from "moment";
+import "moment-timezone";
+import events from "./events";
+import "react-big-calendar/lib/css/react-big-calendar.css";
 import "./Schedule.css";
 
+import TimezoneSelect from "./TimezoneSelect";
+
+const localizer = momentLocalizer(moment);
+
+const defaultTZ = moment.tz.guess();
+const defaultDateStr = "2015-4-13";
+
+function getDate(str, momentObj) {
+    return momentObj(str, "YYYY-MM-DD").toDate();
+}
+
 const Schedule = () => {
+    const [timezone, setTimezone] = useState(defaultTZ);
+
+    const { localizer, defaultDate, scrollToTime, myEvents, getNow } = useMemo(() => {
+        moment.tz.setDefault(timezone);
+        return {
+            localizer: momentLocalizer(moment),
+            defaultDate: getDate(defaultDateStr, moment),
+            scrollToTime: moment().toDate(),
+            getNow: () => moment().toDate(),
+            myEvents: [...events],
+        };
+    }, [timezone]);
+
+    useEffect(() => {
+        return () => {
+            moment.tz.setDefault(); // reset to browser TZ on unmount
+        };
+    }, []);
+
     return (
         <div className="schedule">
-            <div className="schedule-time">TIME</div>
             <div className="schedule-cont">
                 <div className="schedule-day">
                     <Tabs>
@@ -17,13 +51,41 @@ const Schedule = () => {
                         </TabList>
 
                         <TabPanel>
-                            <div>Opening Ceremony</div>
+                            <TimezoneSelect defaultTZ={defaultTZ} timezone={timezone} setTimezone={setTimezone} />
+                            <Calendar
+                                defaultView={Views.DAY}
+                                views={{ day: true }}
+                                localizer={localizer}
+                                events={events}
+                                startAccessor="start"
+                                endAccessor="end"
+                                style={{ height: 800 }}
+                                defaultDate={new Date(2022, 2, 11)}
+                            />
                         </TabPanel>
                         <TabPanel>
-                            <div>Any content 2</div>
+                            <Calendar
+                                defaultView={Views.DAY}
+                                views={{ day: true }}
+                                localizer={localizer}
+                                events={events}
+                                startAccessor="start"
+                                endAccessor="end"
+                                style={{ height: 800 }}
+                                defaultDate={new Date(2022, 2, 12)}
+                            />
                         </TabPanel>
                         <TabPanel>
-                            <div>Any content 2</div>
+                            <Calendar
+                                defaultView={Views.DAY}
+                                views={{ day: true }}
+                                localizer={localizer}
+                                events={events}
+                                startAccessor="start"
+                                endAccessor="end"
+                                style={{ height: 800 }}
+                                defaultDate={new Date(2022, 2, 13)}
+                            />
                         </TabPanel>
                     </Tabs>
                 </div>
@@ -33,3 +95,26 @@ const Schedule = () => {
 };
 
 export default Schedule;
+
+// import { Calendar, momentLocalizer, Views } from "react-big-calendar";
+// import moment from "moment";
+// import events from "./events";
+// import "react-big-calendar/lib/css/react-big-calendar.css";
+// import "./Schedule.css";
+
+// const localizer = momentLocalizer(moment);
+
+// const Schedule = () => (
+//     <div className="schedule">
+//         <Calendar
+//             defaultView={Views.DAY}
+//             views={{ day: true }}
+//             localizer={localizer}
+//             events={events}
+//             startAccessor="start"
+//             endAccessor="end"
+//             style={{ height: 800 }}
+//         />
+//     </div>
+// );
+// export default Schedule;
