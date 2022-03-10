491c07f7c273daf00034fe46bb5b3b52d709f06e
Anmol Burmy <anmolburmy145@gmail.com>
2022-03-10 11:05:30 -0800

    finishing up schedule

--- app/src/Screens/Schedule/Schedule.js
+++ app/src/Screens/Schedule/Schedule.js
@@ -1,3 +1,4 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
 import React from "react";
 import { Calendar, momentLocalizer, Views } from "react-big-calendar";
 import { useState, useEffect } from "react";
@@ -9,6 +10,8 @@ import "./Schedule.css";
 import Popup from "reactjs-popup";
 import events from "./events";
 import { BsLink } from "react-icons/bs";
+import { MdExpandMore } from "react-icons/md";
+import ReactTooltip from "react-tooltip";
 
 const localizer = momentLocalizer(moment);
 
@@ -30,27 +33,39 @@ function Event({ event }) {
 
     return (
         <div className="event-info">
-            <div className="event-title">{event.title}</div>
+            <div className="event-title">
+                {event.title}
+                <Popup
+                    trigger={
+                        <div className="event-desc">
+                            <MdExpandMore />
+                        </div>
+                    }
+                    position="bottom center"
+                    nested
+                >
+                    <div className="event-desc-txt">{event.desc}</div>
+                </Popup>
+            </div>
+
             <div className="event-time">{event.time}</div>
 
-            {/* <Popup
-                trigger={
-                    <a
-                        href="https://docs.google.com/forms/d/e/1FAIpQLSdRys5TN1QxeRkkeBO8TJAamAmD00x0XO8C1YI2FiV_KJ2AEw/viewform"
-                        target="_blank"
-                        rel="noreferrer"
-                        className="event-link"
-                    >
-                        <div className="event-link-txt">Join</div>
-
-                        <BsLink />
-                    </a>
-                }
-                position="top center"
-                on={["hover", "focus"]}
+            <a data-tip data-for="link">
+                <a href={event.link} target="_blank" rel="noreferrer" className="event-link">
+                    <BsLink />
+                </a>
+            </a>
+            <ReactTooltip
+                id="link"
+                aria-haspopup="true"
+                role="example"
+                className="event-link-pop"
+                textColor="#1c1818"
+                backgroundColor="#f5f5f5"
+                arrowColor="#1c1818"
             >
-                <div className="event-link-pop"> Join Workshop</div>
-            </Popup> */}
+                Join
+            </ReactTooltip>
         </div>
     );
 }
@@ -122,7 +137,7 @@ const Schedule = () => {
                                 events={events}
                                 defaultDate={new Date(2022, 2, 11)}
                                 min={new Date(2022, 2, 11, 16, 0, 0)}
-                                max={new Date(2022, 2, 11, 19, 0, 0)}
+                                max={new Date(2022, 2, 11, 20, 0, 0)}
                             />
                         </TabPanel>
                         <TabPanel>
@@ -139,7 +154,7 @@ const Schedule = () => {
                                 events={events}
                                 defaultDate={new Date(2022, 2, 12)}
                                 min={new Date(2022, 2, 12, 8, 0, 0)}
-                                max={new Date(2022, 2, 12, 18, 30, 0)}
+                                max={new Date(2022, 2, 12, 18, 0, 0)}
                             />
                         </TabPanel>
                         <TabPanel>
@@ -156,7 +171,7 @@ const Schedule = () => {
                                 events={events}
                                 defaultDate={new Date(2022, 2, 13)}
                                 min={new Date(2022, 2, 13, 8, 0, 0)}
-                                max={new Date(2022, 2, 13, 18, 30, 0)}
+                                max={new Date(2022, 2, 13, 19, 0, 0)}
                             />
                         </TabPanel>
                     </Tabs>
